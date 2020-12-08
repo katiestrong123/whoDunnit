2debfd8c7902aa613f6a05f7887a5223a83ebdb6
Katie Strong <katiestrong123@gmail.com>
2020-12-08 08:23:41 +0000

    MVP

---
+++ whoDunnit.js
@@ -0,0 +1,202 @@
+// Episode 1
+
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
+  
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+//   The murderer is Miss Scarlett
+
+// Episode 2
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The murderer is Professor Plum because the variable has been declared with const key word and can't be changed within a function.
+
+// Episode 3
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// The first murderer is Mrs Peacock, the let variable inside the declareMurderer function is alterable. The second murderer is Professor Plum, the secondVerdict variable uses the original murderer variable. 
+
+// Episode 4
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// The suspects are Miss Scarlet, Professor Plum and Colonel Mustard in the first block. Inthe second block suspect 3 is Mrs. Peacock because it is a global variable.
+
+// Episode 5
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+// The weaponis the weapon. Theconstant key values can be changed so it has been updated.
+
+// Episode 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The murderer is Mrs White as changemurdered and plot twist are global
+
+// Episode 7
+
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Declare murderer is Miss Scarlet but change murderer is Colonel Mustard.This is only within the change murderer block as it is a local value.
+
+// Episode 8
+
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+
+// "The weapon is Candle Stick."
+// In 'changeScenario' the room value is changed to the string 'Dining Room', which is the string passed as an argument to the nested 'plotTwist' which checks the argument against the room change in 'changeScenario'. We then change the murderer to 'Colonel Mustard', which is also passed into 'unexpectedOutcome' function. This function checks the argument is the same as the value held in the murderer key of the scenario object and changes the weapon to "Candle Stick".
+
+// Episode 9
+
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// "The murderer is Professor Plum" The if statement sets murderer to 'Mrs Peacock', but only within the if statement block.
